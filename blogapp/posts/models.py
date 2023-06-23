ac66b0c384fa2a4fb93e2e518c11de272aea4fdd
ketann26 <ketan9sharma@yahoo.in>
2023-06-23 21:34:59 +0530

    Finshed Blog

--- blogapp/posts/models.py
+++ blogapp/posts/models.py
@@ -2,20 +2,31 @@ from django.db import models
 from django.urls import reverse
 from django.db.models.signals import pre_save
 from django.utils.text import slugify
+from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
+
+class PostManager(models.Manager):
+    def active(self,*args,**kwargs):
+        return super(PostManager,self).filter(draft=False,publish__lte=timezone.now())
 
 def upload_location(instance,filename):
     return "%s/%s" %(instance.id,filename)
 
 class Post(models.Model):
 
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,default=1,on_delete=models.CASCADE)
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True)
     image = models.ImageField(upload_to=upload_location,null=True,blank=True)
     content = models.TextField()
+    draft = models.BooleanField(default=False)
+    publish = models.DateField(auto_now=False, auto_now_add=False)
     updated = timestamp = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    objects = PostManager()
 
     def __str__(self):
         return self.title
--- blogapp/posts/views.py
+++ blogapp/posts/views.py
@@ -1,7 +1,12 @@
+from urllib.parse import quote_plus
+
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import messages
 from django.core.paginator import Paginator
+from django.http import Http404
+from django.utils import timezone
+from django.db.models import Q
 
 from .models import Post
 from .forms import PostForm
@@ -10,9 +15,12 @@ from .forms import PostForm
 
 def posts_create(request):
     
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
+        instance.user = request.user
         instance.save()
         messages.success(request,"Successfully Created")
 
@@ -27,17 +35,32 @@ def posts_create(request):
 def posts_detail(request,slug=None):
 
     instance = get_object_or_404(Post,slug=slug)
+    if instance.draft or instance.publish > timezone.now().date():
+        if not request.user.is_staff or not request.user.is_superuser:
+            raise Http404
+        
     context = {
         'instance': instance,
-        'title': instance.title
+        'title': instance.title,
     }
 
     return render(request,"post_detail.html",context)
 
 def posts_list(request):
 
-    queryset_list = Post.objects.all().order_by('-timestamp')
-    paginator = Paginator(queryset_list, 5)
+    queryset_list = Post.objects.active().order_by('-timestamp')
+    if request.user.is_staff or request.user.is_superuser:
+        queryset_list = Post.objects.all().order_by('-timestamp')
+
+    search_query = request.GET.get('q')
+    if search_query:
+        queryset_list = queryset_list.filter(
+            Q(title__icontains=search_query) |
+            Q(content__icontains=search_query) |
+            Q(user__first_name__icontains=search_query)
+            ).distinct()
+        
+    paginator = Paginator(queryset_list, 2)
 
     page_req_var = "page"
     page_number = request.GET.get(page_req_var)
@@ -53,6 +76,9 @@ def posts_list(request):
 
 
 def posts_update(request,slug=None):
+
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     
     instance = get_object_or_404(Post,slug=slug)
     form = PostForm(request.POST or None, request.FILES or None,instance=instance)
@@ -73,6 +99,9 @@ def posts_update(request,slug=None):
 
 def posts_delete(request,slug=None):
 
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
+    
     instance = get_object_or_404(Post,slug=slug)
     instance.delete()
     messages.success(request,"Successfully Deleted")
